70dace2e06612a5759c33c18b02481585a1a45dc
mgmg <jeonghoson@postech.ac.kr>
2024-06-16 03:56:57 +0900

    feat: allow users to import models from local files

--- dance-animation-with-3d-character/main.js
+++ dance-animation-with-3d-character/main.js
@@ -1,4 +1,14 @@
 import './style.css';
-import { setUpCanvas } from './src/canvas.js';
+import {CanvasController} from './src/canvas.js';
 
-setUpCanvas(document.querySelector('#renderCanvas'));
+const canvasController = new CanvasController(document.querySelector('#renderCanvas'));
+canvasController.setUpCanvas('/Kachujin.glb', '.glb');
+
+const inputModel = document.querySelector('#inputModel');
+inputModel.addEventListener('change', function () {
+    const file = inputModel.files[0];
+    const inputModelPath = URL.createObjectURL(file);
+    const extension = '.' + file.name.split('.').pop();
+
+    canvasController.changeModel(inputModelPath, extension);
+});
--- dance-animation-with-3d-character/src/canvas.js
+++ dance-animation-with-3d-character/src/canvas.js
@@ -3,47 +3,87 @@
 import * as BABYLON from '@babylonjs/core';
 import '@babylonjs/loaders';
 
-/**
- *
- * @param {HTMLCanvasElement} canvas
- */
-export function setUpCanvas(canvas) {
-    const engine = new BABYLON.Engine(canvas, true);
-    const scene = createScene(engine);
+export class CanvasController {
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    #canvas;
+    /**
+     * @type {BABYLON.Engine}
+     */
+    #engine;
 
-    // TODO: need to wait until the SceneLoader in creatScene is complete
-    // Otherwise, scene.render(); is called before the camera is created.
-    // Temporarily fixed the issue creating camera twice.
-    engine.runRenderLoop(function () {
-        scene.render();
-    })
+    /**
+     * Constructor of CanvasController
+     * Must call setUpCanvas() after creating an instance
+     * Side effect: change this.#canvas
+     * @param {HTMLCanvasElement} canvas
+     */
+    constructor(canvas) {
+        this.#canvas = canvas;
+    }
 
-    window.addEventListener('resize', function () {
-        engine.resize();
-    });
-}
+    /**
+     * Asynchronously set up the canvas using the model from the modelPath
+     * Side effect: change this.#engine
+     * @param {String} modelPath
+     * @param {String} extension
+     * @returns {Promise<void>}
+     */
+    async setUpCanvas(modelPath, extension) {
+        const engine = await this.#createEngine();
+        this.#engine = engine;
+        await this.changeModel(modelPath, extension);
 
-/**
- *
- * @param {BABYLON.Engine} engine
- * @returns {BABYLON.Scene}
- */
-function createScene(engine) {
-    const scene = new BABYLON.Scene(engine);
+        window.addEventListener('resize', function () {
+            engine.resize();
+        });
+    }
 
-    scene.createDefaultCamera(true, true, true);
-    BABYLON.SceneLoader.Append("/adamHead/", "adamHead.gltf",
-        scene, createCameraAndLightAndEnvironment);
+    /**
+     * Asynchronously change the model of the canvas to the model from the modelPath
+     * Side effect: change this.#engine
+     * @param {String} modelPath
+     * @param {String} extension
+     * @returns {Promise<void>}
+     */
+    async changeModel(modelPath, extension) {
+        this.#engine.stopRenderLoop();
+        const scene = await this.#createScene(modelPath, extension);
 
-    return scene;
-}
+        this.#engine.runRenderLoop(function () {
+            scene.render();
+        });
+    }
 
-/**
- * 
- * @param {BABYLON.Scene} scene 
- */
-function createCameraAndLightAndEnvironment(scene) {
-    scene.createDefaultCamera(true, true, true);
-    scene.createDefaultLight(true);
-    scene.createDefaultEnvironment({ groundColor: BABYLON.Color3.White() });
+    /**
+     * Asynchronously create a new BABYLON.Engine using this.#canvas
+     * @returns {Promise<BABYLON.Engine>}
+     */
+    async #createEngine() {
+        const engine = new BABYLON.Engine(this.#canvas, true);
+        return engine;
+    }
+
+    /**
+     * Asynchronously create a new BABYLON.Scene using the model from the modelPath
+     * @param {String} modelPath
+     * @param {String} extension
+     * @returns {Promise<BABYLON.Scene>}
+     */
+    async #createScene(modelPath, extension) {
+        const scene = await BABYLON.SceneLoader.LoadAsync(modelPath, "", this.#engine, null, extension);
+        this.#createCameraAndLightAndEnvironment(scene);
+        return scene;
+    }
+
+    /**
+     * @param {BABYLON.Scene} scene
+     * @returns {void}
+     */
+    #createCameraAndLightAndEnvironment(scene) {
+        scene.createDefaultCamera(true, true, true);
+        scene.createDefaultLight(true);
+        scene.createDefaultEnvironment({groundColor: BABYLON.Color3.White()});
+    }
 }
